5dfe3577bda9648e7f3c56a32605ce3d1eace51a
Rashmika Gandra <rashmikagandra@Rashmikas-MacBook-Pro.local>
2026-05-07 11:02:21 +0530

    Creates a project setup
    * Adds source root
    * Adds test root

---
+++ test/AppTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void appTest(){
+        assertEquals(1, 1);
+    }
+}
